4ed8a5a6b8af87c74dadc344ecaf74cb5f0d0d2d
Alex Nunes <alexgaribalde_tb@hotmail.com>
2022-10-12 06:30:44 -0300

    Created DTO

--- backend/src/main/java/com/AlexNunes/dsclient/resources/ClientResource.java
+++ backend/src/main/java/com/AlexNunes/dsclient/resources/ClientResource.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.AlexNunes.dsclient.entities.Client;
+import com.AlexNunes.dsclient.dto.ClientDTO;
 import com.AlexNunes.dsclient.services.ClientService;
 
 @RestController
@@ -19,8 +19,8 @@ public class ClientResource {
 	private ClientService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Client>> findAll(){
-		List<Client> list = service.findAll();
+	public ResponseEntity<List<ClientDTO>> findAll(){
+		List<ClientDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 		}
 }
