248c494b5ded839564efed4aa9f2d3e8bf035c65
Alex Nunes <alexgaribalde_tb@hotmail.com>
2022-10-13 06:16:06 -0300

    Find Client by Id, exception handling

---
+++ backend/src/main/java/com/AlexNunes/dsclient/resources/exceptions/ResourceExcepionHandler.java
@@ -0,0 +1,28 @@
+package com.AlexNunes.dsclient.resources.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.AlexNunes.dsclient.services.exceptions.EntityNotFoundException;
+
+@ControllerAdvice
+public class ResourceExcepionHandler {
+
+	@ExceptionHandler(EntityNotFoundException.class)
+	public ResponseEntity<StandartError> entityNotFound(EntityNotFoundException e, HttpServletRequest request){
+		StandartError err = new StandartError();
+		err.setTimestamp(Instant.now());
+		err.setError("Resource not found");
+		err.setStatus(HttpStatus.NOT_FOUND.value());
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+	
+}
