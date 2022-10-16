747ea8e66732939bf558fb38f37f7988a14ff600
Alex Nunes <alexgaribalde_tb@hotmail.com>
2022-10-16 06:31:40 -0300

    Update client

--- backend/src/main/java/com/AlexNunes/dsclient/resources/exceptions/ResourceExcepionHandler.java
+++ backend/src/main/java/com/AlexNunes/dsclient/resources/exceptions/ResourceExcepionHandler.java
@@ -9,13 +9,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.AlexNunes.dsclient.services.exceptions.EntityNotFoundException;
+import com.AlexNunes.dsclient.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
 public class ResourceExcepionHandler {
 
-	@ExceptionHandler(EntityNotFoundException.class)
-	public ResponseEntity<StandartError> entityNotFound(EntityNotFoundException e, HttpServletRequest request){
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandartError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request){
 		StandartError err = new StandartError();
 		err.setTimestamp(Instant.now());
 		err.setError("Resource not found");
